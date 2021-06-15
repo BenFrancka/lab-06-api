928459b88f8971bdb4dae6460b649da308aeb6ee
BenFrancka <benfrancka@gmail.com>
2021-06-15 13:44:32 -0700

    adds endpoints for all meals and meal by id

--- app.js
+++ app.js
@@ -5,5 +5,17 @@ const app = express();
 
 app.use(cors());
 
+app.get('/meals', (req, res) => {
 
+    res.json(meals);
+})
 
+app.get('/meals:id', (req, res) => {
+    const id = req.params.id;
+
+    const meal= meals.find((mealItem) => mealItem.id === id);
+
+    res.json(meal);
+})
+
+module.exports = app;
