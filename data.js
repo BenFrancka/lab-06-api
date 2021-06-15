041841f1f0c85d8c56245fae8df15e683f4c10af
BenFrancka <benfrancka@gmail.com>
2021-06-15 13:19:35 -0700

    adds data.js

---
+++ data.js
@@ -0,0 +1,68 @@
+meals: [
+
+
+{
+    id: 1,
+    name: 'Chicken Enchiladas',
+    in_stock : true,
+    description: 'Blue corn tortillas with pulled chicken, chile verde sauce, cotija cheese, and fresh cilantro',
+    category: 'Mexican',
+   difficulty: 'easy'
+},
+
+{
+    id: 2,
+    name: 'Spaghetti with Pork Meatballs',
+    in_stock : false,
+    description: 'Handmade spaghetti noodles with classic red sauce, pork meatball kit, fresh basil, and parmigiano reggiano',
+    category: 'Italian',
+    difficulty: 'medium'
+},
+
+{
+    id: 3,
+    name: 'Bison Cheeseburgers',
+    in_stock : true,
+    description: 'Grass fed bison patties with brioche buns, smoked gouda cheese, and quick pickle kit',
+    category: 'American',
+    difficulty: 'medium'
+},
+
+{
+    id: 4,
+    name: 'Spicy Shoyu Ramen',
+    in_stock : false,
+    description: 'Gourmet Instant Ramen kit with pork belly, chili oil, and eggs',
+    category: 'Japanese',
+    difficulty: 'hard'
+},
+
+{
+    id: 5,
+    name: 'Spicy Tuna Rolls',
+    in_stock : true,
+    description: 'Sushi rolling kit (includes bamboo mat), albacore belly tuna, rice, nori, hot sauce, black sesame seeds, scallions',
+    category: 'Japanese',
+    difficulty: 'hard'
+},
+
+{
+    id: 6,
+    name: 'Pepperoni Pizza',
+    in_stock : true,
+    description: 'Detroit style deep dish pizza, includes dough kit, sauce, buffalo mozzarella, and aged pepperoni',
+    category: 'American/Italian',
+    difficulty: 'easy'
+},
+
+{
+    id: 7,
+    name: 'Veggie Tacos',
+    in_stock : false,
+    description: 'Corn tortillas, fire-roasted salsa, zuchinni, nopalitos, mushrooms, lime, and fresh cilantro',
+    category: 'Mexican',
+    difficulty: 'medium'
+}
+]
+
+module.exports = meals;
